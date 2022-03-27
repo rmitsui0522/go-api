3797678a61d80c0549a66ab6b8aeb58700a7356a
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-03-27 23:35:13 +0900

    Fix: ユーザー定義を修正

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -8,10 +8,12 @@ import (
 )
 
 type User struct {
-	ID        uint      `json:"id"`
-	UserName  string    `json:"userName"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID          uint      `json:"id"`
+	FirstName   string    `json:"firstName"`
+	LastName    string    `json:"lastName"`
+	MailAddress string    `json:"mailAddress"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 func (h *handler) getUsers(c echo.Context) error {
@@ -25,13 +27,3 @@ func (h *handler) getUsers(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, users)
 }
-
-// SQL
-// CREATE TABLE `users` (
-// 	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "ユーザーID",
-// 	`username` VARCHAR(100) NOT NULL COMMENT "ユーザー名",
-// 	`createdAt` timestamp DEFAULT NULL COMMENT "登録日",
-// 	`updatedAt` datetime DEFAULT NULL COMMENT "更新日"
-// );
-
-// INSERT INTO users (username, createdAt, updatedAt) VALUES ("test user", now(), now());

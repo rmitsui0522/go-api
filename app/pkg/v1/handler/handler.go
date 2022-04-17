fcbc194625527e14a03c9dad2b4f739f59af9318
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-04-17 23:11:37 +0900

    update: handler
    http.Handlerの定義をcontrollerの責務に変更

--- app/pkg/v1/handler/handler.go
+++ app/pkg/v1/handler/handler.go
@@ -9,29 +9,14 @@ import (
 
 const BASE_URL = "/api/v1"
 
-type handler struct{}
-
 func New() http.Handler {
 	e := echo.New()
+
 	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	h := &handler{}
-
-	e.GET("/health", h.health)
-
-	api := e.Group(BASE_URL)
-
-	api.POST("/auth", h.auth)
-
-	api.GET("/users", h.getUsers)
-	api.POST("/users", h.createUser)
-	api.GET("/users/:id", h.getUser)
-	api.PUT("/users/:id", h.updateUser)
-	api.DELETE("/users/:id", h.deleteUser)
+	initRouting(e)
 
 	return e
-}
-
-func (h *handler) health(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{"message": "OK"})
 }
